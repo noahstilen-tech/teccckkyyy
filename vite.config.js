ff7ab81641b457cd5f29b410fc9bfee49cd88b5e
noahstilen-tech <noahstilen@gmail.com>
2025-12-11 01:05:38 +0100

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '/tmp/cc-agent/61349991/project',
+  resolve: {
+    preserveSymlinks: false,
+  },
 })
